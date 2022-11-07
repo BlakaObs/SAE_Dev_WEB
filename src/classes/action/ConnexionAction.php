8c6deca5e45234640d5ccb38d072cc8c4010459b
BlakaObs <brenann.joly2@etu.univ-lorraine.fr>
2022-11-07 15:27:22 +0100

    Création de la classe ConnexionAction et des classe autour

--- src/classes/action/ConnexionAction.php
+++ src/classes/action/ConnexionAction.php
@@ -2,11 +2,32 @@
 
 namespace sae\web\action;
 
+use sae\web\exception\MotDePasseException;
+use sae\web\authentification\Authentification;
+
 class ConnexionAction extends Action
 {
     public function execute(): string
     {
-        // TODO: Implement execute() method.
-        return "TODO";
+        $res = "";
+        $authHTML = <<<HTML
+            <form action="?action=${_GET['action']}" method="post">
+                <label>Email: </label><input type="text" name="email" placeholder="toto@gmail.com" required>
+                <label>Password: </label><input type="password" name="password" required>
+                <button type="submit">Validate</button>
+            </form>
+        HTML;
+        if ($_SERVER['REQUEST_METHOD'] === "GET") echo $authHTML;
+        else {
+            $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+            $passwd = filter_var($_POST['password']);
+            try {
+                Authentification::authenticate($email,$passwd);
+            }catch (MotDePasseException $e){
+                return "Mot de passe ou email erroné.</br><a href='index.php'>Retour a l'accueil</a>";
+            }
+            $res = "Connection réussite.</br><a href='index.php'>Retour a l'accueil</a>";
+        }
+        return $res;
     }
 }
---
+++ src/classes/exception/MotDePasseException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace sae\web\exception;
+
+class MotDePasseException extends \Exception
+{
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
